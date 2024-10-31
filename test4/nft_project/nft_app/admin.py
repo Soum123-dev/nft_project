17d0292559dd1ae2b3a018ab214a49b4ea3e1567
Soumaila Bissiri <soumailabissiri5@gmail.com>
2024-10-31 12:15:24 +0000

    Ajout d'un nouveau dossier 'test4' contenant un prototype de notre solution de nft

---
+++ test4/nft_project/nft_app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import Etudiant, NFT
+
+# Register your models here.
+
+
+
+admin.site.register(Etudiant)
+admin.site.register(NFT)
